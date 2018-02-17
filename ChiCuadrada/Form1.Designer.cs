7156afa4ec854d920efc0864762539e9b4e6c26d
Cesar Bautista <cbautistac88@gmail.com>
2018-02-17 11:56:41 -0600

    Enviando compilacion de prueba

--- ChiCuadrada/Form1.Designer.cs
+++ ChiCuadrada/Form1.Designer.cs
@@ -221,7 +221,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Chi-Cuadrada de Prueba en Git";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
